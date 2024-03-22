e855e3b336c64dd9e95eedcce1ddba1e79b32c63
Kev-rod43 <kev.rod43@yahoo.com>
2024-03-21 23:09:57 -0400

    fixed some formatting issues

--- models/Category.js
+++ models/Category.js
@@ -2,21 +2,21 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
-    id:{
+    id: {
       type: DataTypes.INTEGER,
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-  },
+    },
 
-  category_name:{
+    category_name: {
       type: DataTypes.STRING,
       allowNull: false,
-  },
+    },
   },
   {
     sequelize,
